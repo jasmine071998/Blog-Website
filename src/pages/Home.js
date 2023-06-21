e0cbebc30d122e2d37d23588d13a0b883111f3c6
Jasmine <jasminepinto19@gmail.com>
2023-06-22 00:23:16 +0530

    Blog files added

---
+++ src/pages/Home.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+import { async } from "@firebase/util";
+import { useNavigate } from "react-router-dom";
+
+function Home({ isAuth }) {
+  let navigate = useNavigate();
+
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id);
+    await deleteDoc(postDoc);
+    navigate("/");
+  };
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+
+    getPosts();
+  }, [deletePost]);
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1> {post.title}</h1>
+              </div>
+              <div className="deletePost">
+                {isAuth && post.author.id === auth.currentUser.uid && (
+                  <button
+                    onClick={() => {
+                      deletePost(post.id);
+                    }}
+                  >
+                    {" "}
+                    &#128465;
+                  </button>
+                )}
+              </div>
+            </div>
+            <div className="postTextContainer"> {post.postText} </div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
